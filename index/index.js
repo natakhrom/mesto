6c61e6a8072b41f2883e3c5cc75b4b3f794548ee
Natalia Khromova <natakhrom2701@yandex.ru>
2020-04-15 11:44:48 +0300

    Контрольная работа 4. Проверкаю 2

--- index/index.js
+++ index/index.js
@@ -1,4 +1,4 @@
-const editButton = document.querySelector('.profile__button-edit');
+const editButton = document.querySelector('.profile__button_edit'); 
 const closeButton = document.querySelector('.popup__close-icon');
 let popupForm = document.querySelector('.popup');
 let nameInput = document.querySelector('.popup__info_name');
@@ -6,23 +6,25 @@ let jobInput = document.querySelector('.popup__info_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__text');
 
-editButton.addEventListener('click', showProfileEditPopUp);
+editButton.addEventListener('click', function () { showCloseProfileEditPopUp(true); }); // событие по нажатию кнопки редактирования
+closeButton.addEventListener('click', function () { showCloseProfileEditPopUp(false); }); // событие по нажатию кнопки закрыть (Х)
 
-function showProfileEditPopUp() {
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
-
-    popupForm.classList.add('popup_opened');
-}
-
-closeButton.addEventListener('click', closeProfileEditPopUp);
-
-function closeProfileEditPopUp() {
-    popupForm.classList.remove('popup_opened');
-}
+function showCloseProfileEditPopUp(show) {
+    if (show) {
+        nameInput.value = nameProfile.textContent;
+        jobInput.value = jobProfile.textContent;
+        popupForm.classList.remove('popup_closed');
+        popupForm.classList.add('popup_opened');
+    }
+    else {
+        popupForm.classList.remove('popup_opened');
+        popupForm.classList.add('popup_closed');       
+    }
+}   
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
+
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
@@ -33,9 +35,9 @@ function formSubmitHandler (evt) {
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
 
-    closeProfileEditPopUp();
+    showCloseProfileEditPopUp(false); //событие по нажатию кнопки "Сохранить"
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-popupForm.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler); 
